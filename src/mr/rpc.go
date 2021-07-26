8a783bfb34d9982bba2d28e153f0ab50ca8d8e4d
Beney Chen <beney-chen@qq.com>
2021-07-26 11:55:11 +0800

    do map

--- src/mr/rpc.go
+++ src/mr/rpc.go
@@ -6,8 +6,10 @@ package mr
 // remember to capitalize all names.
 //
 
-import "os"
-import "strconv"
+import (
+	"os"
+	"strconv"
+)
 
 //
 // example to show how to declare the arguments
@@ -24,6 +26,30 @@ type ExampleReply struct {
 
 // Add your RPC definitions here.
 
+type RegisterArgs struct {
+}
+
+type RegisterReply struct {
+	WorkerId int
+}
+
+type TaskArgs struct {
+	WorkerId int
+}
+
+type TaskReply struct {
+	Task *Task
+}
+
+type ReportArgs struct {
+	Done     bool
+	Seq      int
+	Phase    TaskPhase
+	WorkerId int
+}
+
+type ReportReply struct {
+}
 
 // Cook up a unique-ish UNIX-domain socket name
 // in /var/tmp, for the coordinator.
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,10 +1,13 @@
 package mr
 
 import (
+	"encoding/json"
 	"fmt"
 	"hash/fnv"
+	"io/ioutil"
 	"log"
 	"net/rpc"
+	"os"
 )
 
 //
@@ -94,13 +97,13 @@ type Task struct {
 }
 
 // MapFunc takes document name and document content, and
-// produces a set of intermediate KV pairs. It's provided
-// by the user.
+// produces a set of intermediate KV pairs. It should be
+// provided by the user.
 type MapFunc func(name string, content string) []KeyValue
 
 // ReduceFunc accepts an intermediate key and a set of values
 // for that key. It merges together these values to form a result.
-// It's also provided by the user.
+// It should also be provided by the user.
 type ReduceFunc func(key string, values []string) string
 
 // worker can do either mapping or reducing
@@ -112,24 +115,91 @@ type worker struct {
 
 // Registers this worker to the coordinator through RPC.
 func (w *worker) register() {
-
+	args := &RegisterArgs{}
+	reply := &RegisterReply{}
+	if ok := call("Coordinator.RegiterWorker", args, reply); !ok {
+		log.Fatal("[worker.regiter] register fail")
+	}
+	w.id = reply.WorkerId
 }
 
 // Requests a Task from the coordinator through RPC,
 // then returns a Task assigned by the coordinator.
 func (w *worker) request() Task {
+	args := &TaskArgs{WorkerId: w.id}
+	reply := &TaskReply{}
 
+	if ok := call("Coorindator.GetTask", args, reply); !ok {
+		log.Fatal("[worker.request] get task failed")
+		os.Exit(1)
+	}
+	DebugPrintf("worker-%d get task:%+v", w.id, reply.Task)
+	return *reply.Task
+}
+
+// do locally, so use *Task instead of Task
+func (w *worker) doTask(t *Task) {
+	DebugPrintf("worker-%d do task", w.id)
+
+	switch t.Phase {
+	case MapPhase:
+		w.doMap(t)
+	case ReducePhase:
+		w.doReduce(t)
+	default:
+		panic(fmt.Sprintf("task err in phase: %v", t.Phase))
+	}
+}
+
+func (w *worker) doMap(t *Task) {
+	byts, err := ioutil.ReadFile(t.File)
+	if err != nil {
+		w.report(*t, false, err)
+		return
+	}
+
+	// 1. Map to intermediate kv pairs
+	kvs := w.mapFn(t.File, string(byts))
+
+	// 2. Partition intermediates into NReduce regions by hashing.
+	tasks := make([][]KeyValue, t.NReduce)
+	for _, kv := range kvs {
+		i := ihash(kv.Key) % t.NReduce
+		tasks[i] = append(tasks[i], kv)
+	}
+
+	// 3. Marshal into JSON, and write to local disk
+	for i, kvs := range tasks {
+		filename := reduceName(t.Seq, i)
+		f, err := os.Create(filename)
+		if err != nil {
+			w.report(*t, false, err)
+			return
+		}
+		codec := json.NewEncoder(f)
+		for _, kv := range kvs {
+			if err := codec.Encode(&kv); err != nil {
+				w.report(*t, false, err)
+			}
+		}
+		if err := f.Close(); err != nil {
+			w.report(*t, false, err)
+		}
+	}
+	w.report(*t, true, nil)
+}
+
+func (w *worker) doReduce(t *Task) {
+	// 1. Call RPC to read buffered data from map worker's local disk.
+
+	// 2. Sort by keys for grouping.
+
+	// 3. Iterate over the data, pass the key and its corresponding
+	// set of values to the user's reduce function. Append the output
+	// to a final output file for this reduce partition.
 }
 
 // Reports task to the coorindinator through RPC.
 func (w *worker) report(t Task, done bool, err error) {
-
-}
-
-func (w *worker) doMap(t Task) {
-
-}
-
-func (w *worker) doReduce(t Task) {
 
 }
